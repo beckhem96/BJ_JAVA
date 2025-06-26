b16d286774eaa13c1ab0d89fe0be98108b8ac029
beckhem96 <beckhem96@naver.com>
2025-06-26 18:36:30 +0900

    잠시 백업

---
+++ src/baekjoon/graph/b1039.java
@@ -0,0 +1,43 @@
+package baekjoon.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class b1039 {
+    public static void main(String[] arg) throws IOException {
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String N = st.nextToken();
+        int[] nums = new int[N.length()];
+//        boolean[] visited = new boolean[N.length()];
+        int K = Integer.parseInt(st.nextToken());
+        // N이 한자리 수면 끝
+        if (N.length() == 1) {
+            System.out.println(-1);
+            System.exit(0);
+        }
+        if (N.length() == 2 && N.contains("0")) {
+            System.out.println(-1);
+            System.exit(0);
+        }
+        for (int i = 0; i<N.length();i++) {
+            nums[i] = N.charAt(i);
+        }
+        int curIdx = 0;
+        while (K-- > 0) {
+            int chgInx = 0;
+            for (int i = curIdx + 1; i < N.length(); i++) {
+                if (nums[i] >= nums[curIdx]) {
+                    chgInx = i;
+                }
+            }
+            int tmp = nums[curIdx];
+            nums[curIdx] = nums[chgInx];
+            nums[chgInx] = tmp;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
